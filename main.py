a514f611b14620ce54c2f923e0ea955a1ce79653
PyDesk <soumyasouravpatnaik@gmail.com>
2021-05-09 09:56:05 +0530

    Updated the consts and log msg

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from automation.admission import Admission
 from utilties.config import breaker_file, log_file_path
 import os
+import schedule
+import time
 import logging
 import logging.config
 logging.FileHandler(log_file_path, mode='a', encoding=None, delay=False,)
 logging.config.fileConfig('logging.conf')
 logger = logging.getLogger('main')
+
 
 def do_activity():
     logger.info('Start Activity')
@@ -13,9 +16,14 @@ def do_activity():
     ob.Main()
 
 
-while True:
-    if not os.path.exists(breaker_file):
-        do_activity()
-    else:
-        logger.info("Break file detected.. Quitting..")
-        break
+if __name__ == '__main__':
+    while True:
+        if not os.path.exists(breaker_file):
+            try:
+                do_activity()
+            except Exception as e:
+                logger.exception("Exception.. Sleeping for 20 secs")
+                time.sleep(20)
+        else:
+            logger.info("Break file detected.. Quitting..")
+            break
